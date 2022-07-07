db6afd2d90ad6f980a3cfa4ea3b389491bc7971b
DenisDB <denisbocharov12@gmail.com>
2022-07-07 17:14:03 +0300

    master

---
+++ database/seeders/SpatieAdminsAndUsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class SpatieAdminsAndUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+}
